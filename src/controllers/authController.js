761509f08e4733f274c17aba51d683470a143567
natikawrs <natika.wrs@gmail.com>
2022-09-20 15:05:44 +0700

    done login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,56 +2,9 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-// const AppError = require("../utils/appError");
-// const { User } = require("../models");
-
-// const genToken = (payload) =>
-//   jwt.sign(payload, process.env.JWT_SECRET_KEY || "private_key", {
-//     expiresIn: process.env.JWT_EXPIRES || "7d"
-//   });
-
-// exports.register = async (req, res, next) => {
-//   try {
-//     const { firstName, lastName, emailorMobile, password, confirmPassword } =
-//       req.body;
-
-//     if (!emailorMobile) {
-//       //   res.status(400).json({ message: "email or mobile is required" });
-//       throw new AppError("email or mobile is required", 400);
-//     }
-//     if (!password) {
-//       throw new AppError("password is required", 400);
-//     }
-
-//     if (password !== confirmPassword) {
-//       throw new AppError("password and confirmPassword did not match", 400);
-//     }
-
-//     const isEmail = validator.isEmail(emailorMobile + "");
-//     const isMobile = validator.isMobilePhone(emailorMobile + "");
-
-//     if (!isEmail && isMobile) {
-//       throw new AppError("email address or mobile is invalid format", 400);
-//     }
-
-//     const hashedPassword = await bcrypt.hash(password, 12);
-//     const user = await User.create({
-//       firstName,
-//       lastName,
-//       email: isEmail ? emailorMobile : null,
-//       mobile: isMobile ? emailorMobile : null,
-//       password: hashedPassword
-//     });
-
-//     const token = genToken({ id: user.id });
-//     res.status(201).json({ token });
-//   } catch (err) {
-//     next(err);
-//   }
-// };
-
 const AppError = require("../utils/appError");
 const { User } = require("../models");
+const { Op } = require("sequelize");
 
 const genToken = (payload) =>
   jwt.sign(payload, process.env.JWT_SECRET_KEY || "private_key", {
@@ -94,6 +47,36 @@ exports.register = async (req, res, next) => {
 
     const token = genToken({ id: user.id });
     res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { emailOrMobile, password } = req.body;
+
+    if (typeof emailOrMobile !== "string" || typeof password !== "string") {
+      throw new AppError("email address or mobile or password is invalid", 400);
+    }
+
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ email: emailOrMobile }, { mobile: emailOrMobile }]
+      }
+    });
+
+    if (!user) {
+      throw new AppError("email address or mobile or password is invalid", 400);
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+    if (!isCorrect) {
+      throw new AppError("email address or mobile or password is invalid", 400);
+    }
+
+    const token = genToken({ id: user.id });
+    res.status(200).json({ token });
   } catch (err) {
     next(err);
   }
